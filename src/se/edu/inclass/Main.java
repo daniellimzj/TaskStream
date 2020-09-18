fea780b4b4fc8a37260933d559fb108826451747
daniellimzj <daniellimzijie@gmail.com>
2020-09-18 17:03:30 +0800

    add methods for using streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -39,11 +39,36 @@ public class Main {
         }
     }
 
+    public static void printDataUsingStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing data using streams");
+        tasksData.stream()
+                .forEach(System.out::println);
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing deadlines using streams");
+        tasksData.stream()
+                .filter((t) -> t instanceof Deadline)
+                .forEach(System.out::println);
+
+    }
+
+    public static int countDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        System.out.println("Calculating count using streams");
+        int count;
+
+        count = (int)tasksData.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+
+        return count;
     }
 }
